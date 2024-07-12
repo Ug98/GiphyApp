b3ba03b1c1d1bef2b05e9235f895a810f2ead19c
Ug98 <iudjell98@gmail.com>
2024-07-12 16:50:47 +0300

    rend

--- template/src/events/favorites-events.js
+++ template/src/events/favorites-events.js
@@ -1,5 +1,7 @@
 import { addFavorite, getFavorites, removeFavorite } from '../data/favorites.js';
 import { loadSingleGif } from '../requests/request-service.js';
+import { renderFavorites } from './navigation-events.js';
+
 export const toggleFavoriteStatus = async (gifId) => {
   if (!gifId) {
     console.error('Invalid GIF ID:', gifId);
@@ -10,6 +12,7 @@ export const toggleFavoriteStatus = async (gifId) => {
 
   if (favorites.find(fav => fav.id === gifId)) {
     removeFavorite(gifId);
+    await renderFavorites();
   } else {
     const gif = await loadSingleGif(gifId);
     if (gif && gif.images && gif.images.downsized_medium && gif.images.downsized_medium.url) {
