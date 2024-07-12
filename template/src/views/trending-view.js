a238ca1debc729aad8782f1f0bc4ca9ada47e688
Berkan Karaveli <berkankaraveli@abv.bg>
2024-07-13 00:13:56 +0300

    css update

--- template/src/views/trending-view.js
+++ template/src/views/trending-view.js
@@ -5,11 +5,13 @@ export const toTrendingView = (gifs) => `
   <h1>Trending</h1>
   <div class="trending">
     ${gifs.data.map(gif => `
-      <div class="grid-item">
+      <div class="grid-item gif-container">
         <img src="${gif.images.downsized_medium.url}" alt="${gif.title}">
-        <p>${gif.title}</p>
-        <button class="view-trending-btn" data-trending-id="${gif.id}">View info</button>
-        <button class="${renderFavoriteStatus(gif.id) === FULL_HEART ? 'remove-from-favorites' : 'add-to-favorites'}" data-gif-id="${gif.id}">${renderFavoriteStatus(gif.id)}</button>
+        <div class="overlay"></div>
+        <div class="buttons">
+          <button class="view-trending-btn" data-trending-id="${gif.id}">View info</button>
+          <button class="${renderFavoriteStatus(gif.id) === FULL_HEART ? 'remove-from-favorites' : 'add-to-favorites'}" data-gif-id="${gif.id}">${renderFavoriteStatus(gif.id)}</button>
+        </div>
       </div>
     `).join('\n')}
   </div>
