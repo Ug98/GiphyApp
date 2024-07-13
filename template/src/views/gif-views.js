c39d9f9d94b42172ccb3b6fb7dd6afcb3a29f1f5
Berkan Karaveli <berkankaraveli@abv.bg>
2024-07-13 15:10:24 +0300

    hover on gif buttons

--- template/src/views/gif-views.js
+++ template/src/views/gif-views.js
@@ -2,8 +2,7 @@ export const toGifDetailedView = (gif) => `
   <div id="gif-details" class="gif-details">
     <h1>${gif.title}</h1>
     <img src='${gif.images.original.url}' alt='${gif.title}'>
-    <p>Views: ${gif.analytics.onload.url}</p>
-    <p>Username: ${gif.username}</p>
+    <p>Uploaded by: ${gif.username || 'N/A'}</p>
     <button id="close-gif-info">Close</button>
   </div>
 `;
--- template/src/views/trending-view.js
+++ template/src/views/trending-view.js
@@ -5,12 +5,14 @@ export const toTrendingView = (gifs) => `
   <h1>Trending</h1>
   <div class="trending">
     ${gifs.data.map(gif => `
-      <div class="grid-item gif-container">
-        <img src="${gif.images.downsized_medium.url}" alt="${gif.title}">
-        <div class="overlay"></div>
-        <div class="buttons">
-          <button class="view-trending-btn" data-trending-id="${gif.id}">View info</button>
-          <button class="${renderFavoriteStatus(gif.id) === FULL_HEART ? 'remove-from-favorites' : 'add-to-favorites'}" data-gif-id="${gif.id}">${renderFavoriteStatus(gif.id)}</button>
+      <div class="grid-item">
+        <div class="gif-container">
+          <img src="${gif.images.downsized_medium.url}" alt="${gif.title}">
+          <div class="overlay"></div>
+          <div class="buttons">
+            <button class="view-trending-btn" data-trending-id="${gif.id}">View info</button>
+            <button class="${renderFavoriteStatus(gif.id) === FULL_HEART ? 'remove-from-favorites' : 'add-to-favorites'}" data-gif-id="${gif.id}">${renderFavoriteStatus(gif.id)}</button>
+          </div>
         </div>
       </div>
     `).join('\n')}
