33ce27b31525d308d271c1b54b96dc4c7071392a
Berkan Karaveli <berkankaraveli@abv.bg>
2024-07-13 17:47:47 +0300

    all hovers fixed

--- template/src/views/favorites-view.js
+++ template/src/views/favorites-view.js
@@ -1,39 +1,43 @@
-import { FULL_HEART,EMPTY_HEART } from "../common/constants.js";
+import { FULL_HEART, EMPTY_HEART } from "../common/constants.js";
 import { renderFavoriteStatus } from "../events/helpers.js";
 
 export const toFavoritesView = (favorites) => {
- 
-
   if (favorites.length === 0) {
-    return '<h1>No favorites yet</h1>'
+    return '<h1>No favorites yet</h1>';
   } else {
     return `
-    <h1>Favorite gifs:</h1>
-    <div class="trending">
-      ${favorites.map(gif => {
-      return `
-          <div class="grid-item gif-container" data-key="${gif.id}">
-            <img src="${gif.images?.downsized_medium.url}" alt="GIF">
-            <p>${gif.title}</p>
-            <button class="view-trending-btn" data-trending-id="${gif.id}">View info</button>
-            <button class="remove-from-favorites" data-gif-id="${gif.id}">${FULL_HEART}</button>
-          </div>`;
-    }).join('\n')}
-    </div>
-  `;
-
+      <h1>Favorite gifs:</h1>
+      <div class="trending">
+        ${favorites.map(gif => `
+          <div class="grid-item">
+            <div class="gif-container">
+              <img src="${gif.images?.downsized_medium.url}" alt="${gif.title}">
+              <div class="overlay"></div>
+              <div class="buttons">
+                <button class="view-trending-btn" data-trending-id="${gif.id}">View info</button>
+                <button class="remove-from-favorites" data-gif-id="${gif.id}">${FULL_HEART}</button>
+              </div>
+            </div>
+          </div>
+        `).join('\n')}
+      </div>
+    `;
   }
 };
 
 export const toRandomGifsView = (gifs) => `
-  <h1>No favorites yet</h1>
+  <h1>Random gifs:</h1>
   <div id="random-gifs">
     ${gifs.map(gif => `
       <div class="grid-item">
-        <img src="${gif.images.downsized_medium.url}" alt="${gif.title}">
-        <p>${gif.title}</p>
-        <button class="view-trending-btn" data-trending-id="${gif.id}">View info</button>
-        <button class="${renderFavoriteStatus(gif.id) === FULL_HEART ? 'remove-from-favorites' : 'add-to-favorites'}" data-gif-id="${gif.id}">${renderFavoriteStatus(gif.id)}</button>
+        <div class="gif-container">
+          <img src="${gif.images.downsized_medium.url}" alt="${gif.title}">
+          <div class="overlay"></div>
+          <div class="buttons">
+            <button class="view-trending-btn" data-trending-id="${gif.id}">View info</button>
+            <button class="${renderFavoriteStatus(gif.id) === FULL_HEART ? 'remove-from-favorites' : 'add-to-favorites'}" data-gif-id="${gif.id}">${renderFavoriteStatus(gif.id)}</button>
+          </div>
+        </div>
       </div>
     `).join('\n')}
   </div>
--- template/src/views/search-view.js
+++ template/src/views/search-view.js
@@ -2,15 +2,19 @@ import { renderFavoriteStatus } from "../events/helpers.js";
 import { FULL_HEART } from "../common/constants.js";
 
 export const toSearchView = (gifs, searchTerm) => `
-    <h2>Search results for "${searchTerm}":</h2>
-    <div class="trending">
-        ${gifs.data.map(gif => `
-     <div class="grid-item gif-container">
-            <img src="${gif.images.downsized_medium.url}">
+  <h2>Search results for "${searchTerm}":</h2>
+  <div class="trending">
+    ${gifs.data.map(gif => `
+      <div class="grid-item">
+        <div class="gif-container">
+          <img src="${gif.images.downsized_medium.url}" alt="${gif.title}">
+          <div class="overlay"></div>
+          <div class="buttons">
             <button class="view-trending-btn" data-trending-id="${gif.id}">View info</button>
             <button class="${renderFavoriteStatus(gif.id) === FULL_HEART ? 'remove-from-favorites' : 'add-to-favorites'}" data-gif-id="${gif.id}">${renderFavoriteStatus(gif.id)}</button>
-                <p>${gif.title}</p>
+          </div>
         </div>
-              `).join('')}
-    </div>
-  `;
+      </div>
+    `).join('\n')}
+  </div>
+`;
